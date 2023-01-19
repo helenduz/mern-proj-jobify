d72b227f027546850a7c3c7ca388a954f1c4b594
helenduz <duh2@carleton.edu>
2023-01-19 13:18:55 -0500

    Fix typos in auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,8 +3,8 @@ const authRouter = express.Router();
 
 import { register, login, updateUser } from '../controllers/authController.js'
 
-authRouter.route('/login').post(register);
-authRouter.route('/register').post(login);
+authRouter.route('/register').post(register);
+authRouter.route('/login').post(login);
 authRouter.route('/updateUser').patch(updateUser);
 
 export default authRouter;
