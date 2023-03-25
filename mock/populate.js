4f7a2618bc38d08f22a137f26cdfa645aa4cb315
helenduz <duh2@carleton.edu>
2023-03-24 23:41:33 -0400

    Add mock data and populate.js; update Job model field

---
+++ mock/populate.js
@@ -0,0 +1,28 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import { readFile } from "fs/promises";
+import connectDB from "../db/connect.js";
+import Job from "../models/Job.js";
+
+const start = async () => {
+  try {
+    // connect to DB
+    await connectDB(process.env.MONGO_URI);
+    // delete existing jobs
+    await Job.deleteMany();
+    // create jobs with mock data
+    const mockJobsJSON = JSON.parse(
+      await readFile(new URL("./mock_data.json", import.meta.url))
+    );
+    await Job.create(mockJobsJSON);
+    // end the program
+    console.log("Success!");
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(0);
+  }
+};
+
+start();
