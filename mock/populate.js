67ded0564ff175b49a0d2fcd2d1655ca9db0c7b6
helenduz <duh2@carleton.edu>
2023-05-22 22:21:08 +0800

    Demo user data population

--- mock/populate.js
+++ mock/populate.js
@@ -7,18 +7,18 @@ import Job from "../models/Job.js";
 
 const start = async () => {
   try {
-    // connect to DB
-    await connectDB(process.env.MONGO_URI);
-    // delete existing jobs
-    await Job.deleteMany();
-    // create jobs with mock data
-    const mockJobsJSON = JSON.parse(
-      await readFile(new URL("./mock_data.json", import.meta.url))
-    );
-    await Job.create(mockJobsJSON);
-    // end the program
-    console.log("Success!");
-    process.exit(0);
+      // connect to DB
+      await connectDB(process.env.MONGO_URI);
+      // // delete existing jobs
+      // await Job.deleteMany();
+      // create jobs with mock data
+      const mockJobsJSON = JSON.parse(
+          await readFile(new URL("./mock_data.json", import.meta.url))
+      );
+      await Job.create(mockJobsJSON);
+      // end the program
+      console.log("Success!");
+      process.exit(0);
   } catch (error) {
     console.log(error);
     process.exit(0);
