1337deec06798cbaada28427baecf3fbd78035d5
helenduz <duh2@carleton.edu>
2023-06-17 13:52:57 -0400

    Add user checking in landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,25 +1,35 @@
 import main from "../assets/images/main.svg"
 import Wrapper from "../assets/wrappers/LandingPage"
 import { Logo } from "../components/Components"
-import { Link } from "react-router-dom"
+import { Link, Navigate } from "react-router-dom";
+import { useAppContext } from "../context/appContext";
 
 const Landing = () => {
+    const { user } = useAppContext(); //check if user already logged in
     return (
-        <Wrapper>
-            <nav>
-                <Logo/>
-            </nav>
-            <div className="container full-page">
-                <div className="info">
-                    <h1>Job Tracking App</h1>
-                    <p>Descriptions</p>
-                    <Link to="/register" className="btn btn-hero">Login/Register</Link>
+        <>
+            {user && <Navigate to="/" />}
+            <Wrapper>
+                <nav>
+                    <Logo />
+                </nav>
+                <div className="container full-page">
+                    <div className="info">
+                        <h1>Job Tracking App</h1>
+                        <p>Descriptions</p>
+                        <Link to="/register" className="btn btn-hero">
+                            Login/Register
+                        </Link>
+                    </div>
+
+                    <img
+                        src={main}
+                        alt="job hunt image"
+                        className="img main-img"
+                    ></img>
                 </div>
-                
-                <img src={main} alt="job hunt image" className="img main-img"></img>
-                
-            </div>
-        </Wrapper>
+            </Wrapper>
+        </>
     );
 };
 
